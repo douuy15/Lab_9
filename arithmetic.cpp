863ba399378774c66c69a402139779fb09887aaf
GarinFraser <fraser_garin@wheatoncollege.edu>
2025-12-01 16:08:57 -0500

    this is a commit that adds arithmetic prototype and implementation files

---
+++ arithmetic.cpp
@@ -0,0 +1,17 @@
+#include "arithmetic.h"
+float sum(float a, float b){
+    float sum = a + b;
+    return sum;
+}
+float sub(float a, float b){
+    float sub = a - b;
+    return sub;
+}
+float multiply(float a, float b){
+    float multiply = a * b;
+    return multiply;
+}
+float divide(float a, float b){
+    float divide = a / b;
+    return divide;
+}
---
+++ arithmetic.h
@@ -0,0 +1,9 @@
+#ifndef LAB_9_ARITHMETIC_H
+#define LAB_9_ARITHMETIC_H
+#include <iostream>
+
+float sum(float a, float b);
+float sub(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+#endif //LAB_9_ARITHMETIC_H
